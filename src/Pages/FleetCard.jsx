d4a33faa179d8d481f20d8dfe630ea3186a6e60f
sowmya <macharlasowmya8@gmail.com>
2025-12-28 20:37:25 +0530

    fleet-management-react-app

--- src/Pages/FleetCard.jsx
+++ src/Pages/FleetCard.jsx
@@ -1,33 +1,31 @@
-function FleetCard(){
-    const FeltCard=React.memo(function FleetCard({
-        data,
-        onUpdate,
-        onToggle,
-        onDelete
-    })
-    {
-        function updateDriver(){
-            const name=prompt("Enter the driver name");
-            if(name&&name.trim()){
-                onUpdate(data.id,name);
-            }
+function FleetCard() {
+	const FeltCard = React.memo(function FleetCard({data, onUpdate, onToggle, onDelete}) {
+			function updateDriver() {
+				const name = prompt("Enter the driver name");
+				if (name && name.trim()) {
+					onUpdate(data.id, name);
+				}
 
-        }
+			}
 
-    }
-    return(
-       <>
-        <button onClick={updateDriver}>update driver</button>
-        <button onClick={()=>{
-            onToggle(data.id)
-        }
-        }>Toggle</button>
-        <button onClick={()=>{
-            onDelete(data.id)>Delete </button>
-        
-        
-    }
-       </>
-    )
+		},[])
+		return (
+			<>
+				<button onClick={updateDriver}>update driver</button>
+				<button onClick={
+					() => {
+						onToggle(data.id)
+					}
+				}>Toggle</button>
+				<button onClick={
+					() => {
+						onDelete(data.id)
+					}
+				}>Delete</button>
 
-}
+
+			</>
+		)
+
+	}
+)
